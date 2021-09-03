938aa37eca3d603493c48870974bc664b5804147
jenar-k <jenarkuswidiardi@gmail.com>
2021-09-03 19:49:50 +0700

    commit kedua

--- cypress/integration/login/login.js
+++ cypress/integration/login/login.js
@@ -1,21 +1,14 @@
 describe('Login as Admin PKH', function () {
     it('TC-01 login dengan data valid', function () {
-        cy.visit('https://admin.pkh.dojobox.id/');
-        cy.url().should('include', 'admin.pkh.dojobox.id');
-        cy.get('#input-email').type('e@dojobox.id',{force:true}).should('have.value', 'e@dojobox.id');
-        cy.get('#input-password').type('admin',{force:true}).should('have.value', 'admin');
+        cy.mainURL();
+        cy.adminLogin();
         cy.contains('Login').click({force:true});
         cy.get('.mr-2 > b').should('have.text', 'Super Admin');
-        cy.get('#userDropdown').click({force:true});
-        cy.get('.dropdown-menu > [href="#"]').click({force:true})
-        cy.get('#exampleModalLabel').should('have.text', 'Ready to Leave?');
-        cy.get('.modal-footer > .btn-primary').click({force:true});
-        cy.get('.h4').should('have.text', 'Welcome Back!');
+        cy.stepLogout();
     }),
 
     it('TC-02 login dengan data Email Salah', function () {
-        cy.visit('https://admin.pkh.dojobox.id/');
-        cy.url().should('include', 'admin.pkh.dojobox.id');
+        cy.mainURL();
         cy.get('#input-email').type('eh@dojobox.id',{force:true}).should('have.value', 'eh@dojobox.id');
         cy.get('#input-password').type('admin',{force:true}).should('have.value', 'admin');
         cy.contains('Login').click({force:true});
@@ -25,8 +18,7 @@ describe('Login as Admin PKH', function () {
     }),
 
     it('TC-03 login dengan data Password Salah', function () {
-        cy.visit('https://admin.pkh.dojobox.id/');
-        cy.url().should('include', 'admin.pkh.dojobox.id');
+        cy.mainURL();
         cy.get('#input-email').type('e@dojobox.id',{force:true}).should('have.value', 'e@dojobox.id');
         cy.get('#input-password').type('admin123',{force:true}).should('have.value', 'admin123');
         cy.contains('Login').click({force:true});
@@ -36,8 +28,7 @@ describe('Login as Admin PKH', function () {
     }),
 
     it('TC-04 login dengan data semua null', function () {
-        cy.visit('https://admin.pkh.dojobox.id/');
-        cy.url().should('include', 'admin.pkh.dojobox.id');
+        cy.mainURL();
         cy.get('#input-email').click();
         cy.get('#input-password').click();
         cy.contains('Login').click({force:true});
@@ -46,8 +37,7 @@ describe('Login as Admin PKH', function () {
     }),
 
     it('TC-05 login dengan data email null', function () {
-        cy.visit('https://admin.pkh.dojobox.id/');
-        cy.url().should('include', 'admin.pkh.dojobox.id');
+        cy.mainURL();
         cy.get('#input-password').type('admin',{force:true}).should('have.value', 'admin');
         cy.contains('Login').click({force:true});
         cy.get('#exampleModalLabel').should('have.text', 'Error !');
@@ -56,8 +46,7 @@ describe('Login as Admin PKH', function () {
     }),
 
     it('TC-06 login dengan data password null', function () {
-        cy.visit('https://admin.pkh.dojobox.id/');
-        cy.url().should('include', 'admin.pkh.dojobox.id');
+        cy.mainURL();
         cy.get('#input-email').type('e@dojobox.id',{force:true}).should('have.value', 'e@dojobox.id');
         cy.contains('Login').click({force:true});
         cy.get('#exampleModalLabel').should('have.text', 'Error !');
